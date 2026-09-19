9a27a0a0605f35183cefa344c80e36dc726d80b7
sant4304 <140947347+sant4304@users.noreply.github.com>
2026-09-19 17:40:30 +0530

    Clear Redis connection parameters
    Updated Redis configuration to use empty credentials.

--- rev8.2/src/config/cache.js
+++ rev8.2/src/config/cache.js
@@ -2,9 +2,9 @@ const Redis = require("ioredis")
 
 const redis = new Redis(
     {
-     host:"rewarding-boreal-yearlong-60406.db.redis.io",
-     port:"16493",
-     password:"pNN5NdUZREi7Y2NSDoQsEd1kofs7ECMh"
+     host:"",
+     port:"",
+     password:""
     }
 )
 
